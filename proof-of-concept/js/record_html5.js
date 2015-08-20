4e45f9fb0a4ca80e64ad94c730ee769a16be0d5e
Zaid Albirawi <zalbiraw@uwo.ca>
2015-08-20 10:55:28 -0400

    pollyfill

--- proof-of-concept/js/record_html5.js
+++ proof-of-concept/js/record_html5.js
@@ -39,14 +39,13 @@ function stopRecording(button) {
 
 window.onload = function init() {
   try {
-    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    audio_context = (window.AudioContext || window.webkitAudioContext)();
     navigator.getUserMedia = (  navigator.getUserMedia ||
                                 navigator.webkitGetUserMedia ||
                                 navigator.mozGetUserMedia ||
                                 navigator.msGetUserMedia);
     window.URL = window.URL || window.webkitURL;
     
-    audio_context = new AudioContext;
   } catch (e) {
     alert('No web audio support in this browser!');
   }
