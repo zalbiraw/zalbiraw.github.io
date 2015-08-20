5b6650d4b807ead4c7fed1e3366469966a0c498f
Zaid Albirawi <zalbiraw@uwo.ca>
2015-08-20 09:53:37 -0400

    Add HTML5 support

---
+++ proof-of-concept/js/html5/record.js
@@ -0,0 +1,54 @@
+var recorder,
+    r_start   = $('.record-start'),
+    r_stop    = $('.record-stop'),
+    js        = document.createElement("script");
+
+js.type = "text/javascript";
+js.src = "js/html5/recordmp3.js";
+document.body.appendChild(js);
+
+window.onload = function init() {
+  try {
+    navigator.getUserMedia  = ( navigator.getUserMedia       ||
+                                navigator.webkitGetUserMedia ||
+                                navigator.mozGetUserMedia    ||
+                                navigator.msGetUserMedia      );
+
+    window.URL              = ( window.URL                   || 
+                                window.webkitURL              );
+  } catch (e) {
+    alert('No web audio support in this browser!');
+  }
+  
+  navigator.getUserMedia({audio: true}, startUserMedia, function(e) {
+    alert('No live audio input: ' + e);
+  });
+};
+
+function startUserMedia(stream) {
+  var audio_context = new (window.AudioContext || window.webkitAudioContext) ();
+  recorder = new Recorder(audio_context.createMediaStreamSource(stream));
+}
+
+function toggleRecording(state) {
+  if (state)
+  {
+    r_start.css("display", "none");
+    r_stop.css("display", "inline");
+  } else {
+    r_start.css("display", "inline");
+    r_stop.css("display", "none");
+  }
+}
+
+function startRecording(button) {
+  recorder && recorder.record();
+  toggleRecording(true);
+}
+
+function stopRecording(button) {
+  recorder && recorder.stop();
+  toggleRecording(false);
+  recorder && recorder.exportWAV(function(blob) {});
+  recorder.clear();
+}
--- proof-of-concept/js/html5/recordmp3.js
+++ proof-of-concept/js/html5/recordmp3.js
@@ -1,7 +1,7 @@
 (function(window){
 
-  var WORKER_PATH = 'js/recorderWorker.js';
-  var encoderWorker = new Worker('js/mp3Worker.js');
+  var WORKER_PATH = 'js/html5/recorderWorker.js',
+      encoderWorker = new Worker('js/html5/mp3Worker.js');
 
   var Recorder = function(source, cfg){
     var config = cfg || {};
