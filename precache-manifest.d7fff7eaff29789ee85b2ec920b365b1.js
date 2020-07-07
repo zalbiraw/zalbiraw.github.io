1acbccc91c7e67edacc8df43d5ff55c001ca44ec
Zaid Albirawi <zalbiraw@gmail.com>
2020-07-07 01:58:02 -0400

    Add larafood

---
+++ precache-manifest.d7fff7eaff29789ee85b2ec920b365b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f9ade0c34ed9a8fb70ba4102c89e3f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "579907288839a2aa742c",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "527f677d68f6b8ca1c1b",
+    "url": "/static/css/main.29c5b6f2.chunk.css"
+  },
+  {
+    "revision": "579907288839a2aa742c",
+    "url": "/static/js/2.33fb9d30.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.33fb9d30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "527f677d68f6b8ca1c1b",
+    "url": "/static/js/main.f53dfee0.chunk.js"
+  },
+  {
+    "revision": "f6a77e1bed36c8dda9cc",
+    "url": "/static/js/runtime-main.6d3cc023.js"
+  }
+]);
